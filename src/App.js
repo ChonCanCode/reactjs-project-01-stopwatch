29c5347732fc70c87d6e9bef9032c5b8a208d772
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2024-12-22 16:02:39 +0000

    delete

--- src/App.js
+++ src/App.js
@@ -38,21 +38,3 @@ function App() {
 }
 
 export default App;
-
-import React, { useState } from 'react';
-
-function Counter() {
-  // Declare a state variable named "count" and a function to update it called "setCount"
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
-
-export default Counter;
